9f3b2c0624114d64ae436540b982b9250aad002e
tv <tv@krebsco.de>
2018-11-21 01:00:56 +0100

    use curl instead of wget

--- update.sh
+++ update.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-rm -f registry.ets.gz
-wget https://repo.hex.pm/registry.ets.gz
+curl -O https://repo.hex.pm/registry.ets.gz
 git add registry.ets.gz
 git commit -m "$(date)"
 git push origin
